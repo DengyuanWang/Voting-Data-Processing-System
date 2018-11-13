02f42588a6cf1f251a67c1fcb567b29df17ebff2
王登远 <wangdengyuan@ounoborus-MacBook-2.local>
2018-11-12 23:02:02 -0600

    sss

--- Project1/src/ScreenView.java
+++ Project1/src/ScreenView.java
@@ -15,11 +15,11 @@ import javax.swing.SwingConstants;
 
 
 
-public class ScreenView extends javax.swing.JFrame {
+public class ScreenView {
 
     public JFrame frame = new JFrame("Voting System");
 	ScreenView() {
-		frame.setSize(430, 400);
+		frame.setSize(420, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		JPanel panel = new JPanel();
@@ -29,7 +29,7 @@ public class ScreenView extends javax.swing.JFrame {
 		frame.setVisible(true);
 	}
 
-	private void placeComponents(JPanel panel) {
+	private static void placeComponents(JPanel panel) {
 
         panel.setLayout(null);
         
@@ -41,26 +41,11 @@ public class ScreenView extends javax.swing.JFrame {
         // panel.addSeparator();
 
         JTextArea jta = new JTextArea();
-        jta.setSize(400,300);
+        jta.setSize(400,400);
         jta.setLocation(10,40);
         Global.SC.DisplayData();
         String test = Global.SC.Audit.audit_txt;
         jta.setText(test);
         panel.add(jta);
-
-        JButton backButton = new JButton("Back");
-        backButton.setBounds(200,355,100,25);
-        panel.add(backButton);
-
-        ActionListener backButtonListener = new backButtonListener();
-        backButton.addActionListener(backButtonListener);
-    }
-
-    public class backButtonListener extends javax.swing.JFrame implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            FinishProcess back = new FinishProcess();
-            frame.setVisible(false);
-        }
     }
 }
