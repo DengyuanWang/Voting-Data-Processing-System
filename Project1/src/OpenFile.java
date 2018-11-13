c43eb5f517f373d14d6c870d79d352a34d324d72
peterwang0209 <wang5959@umn.edu>
2018-11-12 21:34:24 -0600

    absoluteFilePath

--- Project1/src/OpenFile.java
+++ Project1/src/OpenFile.java
@@ -1,26 +1,47 @@
 import java.util.Scanner;
 import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.filechooser.FileSystemView;
 
     public class OpenFile {
-        JFileChooser fileChooser = new JFileChooser();
+        JFileChooser fileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
         StringBuilder sb = new StringBuilder();
+        String s = "";
 
         public void PickMe() throws Exception{
-            if(fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION){
+            // if(fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION){
 
-                //get the file
-                java.io.File file = fileChooser.getSelectedFile();
+            //     //get the file
+            //     java.io.File file = fileChooser.getSelectedFile();
+            //     s = file.getAbsolutePath();
+            //     System.out.print(s);
+            //     //create a scanner for the file
+            //     Scanner input = new Scanner(file);
 
-                //create a scanner for the file
-                Scanner input = new Scanner(file);
+            //     //read text from file
+            //     while(input.hasNext()){
+            //         sb.append(input.nextLine());
+            //         sb.append("\n");
+            //     }
 
-                //read text from file
-                while(input.hasNext()){
-                    sb.append(input.nextLine());
-                    sb.append("\n");
-                }
+            //     input.close();
 
-                input.close();
+            int r = fileChooser.showOpenDialog(null);
+            
+            if(r == JFileChooser.APPROVE_OPTION){
+                    java.io.File file = fileChooser.getSelectedFile();
+                    // s will store the selected file's path
+                    s = file.getAbsolutePath();
+                    System.out.print(s);
+                    System.out.print("-----------------");
+                    
+                    Scanner input = new Scanner(file);
+
+                    while(input.hasNext()){
+                        sb.append(input.nextLine());
+                        sb.append("\n");
+                    }
+
             }
             else{
                 sb.append("No file was selected");
