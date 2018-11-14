e8b8f5ab8ecab48da008c4b1ef1077e78f2e231f
pentium <wang8662@umn.edu>
2018-11-13 20:41:37 -0600

    remove unnecessary comments

--- Project1/src/Voting_System.java
+++ Project1/src/Voting_System.java
@@ -60,15 +60,12 @@ public class Voting_System {
     public String IR_Voting()
     {
         int[][] Cand_Ballot;    //Cand_Ballot[i][j]: the number of ballots received by candidate#i with ranking#j
-        //int MAXTERM=10000;
         int NUMTERM=0;
-        //auditfile.IRVotingprocess=new IR_frame[MAXTERM];
         Vector IRaudit = new Vector();
         boolean win=false;
         int IRwinner=-1;
         Cand_Ballot=new int[numofcand][numofcand+1];
         do {
-            //auditfile.IRVotingprocess[NUMTERM]=new IR_frame();
             IR_frame tmp=new IR_frame();
             for(int j=0;j<numofballot;j++)
             {
@@ -122,7 +119,6 @@ public class Voting_System {
         }while(!win);
         auditfile.IRVotingprocess=new IR_frame[NUMTERM];
         IRaudit.copyInto(auditfile.IRVotingprocess);
-        //System.out.println(auditfile.IRVotingprocess);
         return Ballotdata.data[0].get_candidate(IRwinner);
     }
     /**
@@ -133,23 +129,18 @@ public class Voting_System {
     public HashMap<String, Integer> OPL_Voting()
     {
         int numofseats=Ballotdata.data[0].get_numofseat();
-        //int[] OPLres=new int[numofcand];        //allocated seats
         int[] OPLvote=new int[numofcand];       //votes
-        //int[] OPLremain=new int[numofcand];     //remaining votes
-        HashMap<String,Integer> OPLcandres=new HashMap<>();
+        HashMap<String,Integer> OPLcandres=new HashMap<>();        //allocated seats
         HashMap<String, String> OPLcandparty=new HashMap<>();
         HashMap<String, Integer> OPLpartyvote=new HashMap<>();
         HashMap<String, Integer> OPLpartyres=new HashMap<>();
-        HashMap<String, Integer> OPLpartyremain=new HashMap<>();
+        HashMap<String, Integer> OPLpartyremain=new HashMap<>();     //remaining votes
         int quota=numofballot/numofseats;
         int undecidedseat=numofseats;
         for(int i=0;i<numofballot;i++)
         {
             int voteidx=Ballotdata.data[i].get_cand_from_rank(1);
             String partyofcand=Ballotdata.data[i].get_party(voteidx);
-            //System.out.print(partyofcand);
-            //System.out.print("--");
-            //System.out.println(Ballotdata.data[i].get_candidate(voteidx));
             OPLpartyvote.put(partyofcand, OPLpartyvote.getOrDefault(partyofcand, 0) + 1);
             OPLvote[voteidx]++;
         }
@@ -172,15 +163,6 @@ public class Voting_System {
         auditfile.OPLVotingprocess[0].partyFinish=false;
         auditfile.OPLVotingprocess[0].PartySeats=(HashMap<String, Integer>) OPLpartyres.clone();
 
-        /*
-        for(int i=0;i<numofcand;i++)        //First Allocation of Seats
-        {
-            OPLres[i]=OPLvote[i]/quota;
-            undecidedseat-=OPLres[i];
-            OPLremain[i]=OPLvote[i]%quota;
-        }
-        */
-
         while(undecidedseat!=0)             //Second Allocation of Seats
         {
             int tmpval=-1;
@@ -196,20 +178,6 @@ public class Voting_System {
                 }
             }
             OPLpartyres.put(tmppty, OPLpartyres.get(tmppty) + 1);
-            //OPLpartyremain.put(tmppty, OPLpartyremain.get(tmppty)-1);
-
-            /*
-            for(int i=0;i<numofcand;i++)
-            {
-                if(OPLremain[i]>tmpval)
-                {
-                    tmpval=OPLremain[i];
-                    tmpidx=i;
-                }
-            }
-            OPLres[tmpidx]++;
-            OPLremain[tmpidx]=-1;
-            */
         }
         for(String party: OPLpartyvote.keySet())
         {
